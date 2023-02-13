fd1d3e6690279ff08ecb4beb08af19ab8664f17f
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-02-13 12:59:42 +0100

    Set to previosly tried solution (Does not work)

--- gruppe22/frontend/src/components/ParagaphParser.tsx
+++ gruppe22/frontend/src/components/ParagaphParser.tsx
@@ -3,9 +3,7 @@ import { Book } from "../types";
 
 import * as ReactDOM from "react-dom";
 
-//This is a great example og worst practise in typescript:
-export function NewlineText(props: string) {
-  const newText = props.split("\n").map((str: string) => <p>{str}</p>);
-
-  return newText;
+export function NewlineText(props: any) {
+  const text = props.text;
+  return <div>{text}</div>;
 }
