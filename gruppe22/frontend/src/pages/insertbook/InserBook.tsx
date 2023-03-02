7823f9559c5f25776de6c801292a3f0b6f3a861b
hermanak <hermanak@stud.ntnu.no>
2023-03-02 14:59:42 +0100

    feat: #18 Incomplete form added to InsertBook
    The form lacks hooks for react and can still not be viewed

--- gruppe22/frontend/src/pages/insertbook/InserBook.tsx
+++ gruppe22/frontend/src/pages/insertbook/InserBook.tsx
@@ -1,1 +1,17 @@
 import React from "react";
+import { useState } from "react";
+
+export default function InsertBook() {
+  return (
+    <form>
+        <div>
+            <label htmlFor="title">Title</label>
+            <input id="title" name="title" type="text"/>
+        </div>
+        <div>
+        <label htmlFor="author">Author</label>
+            <input id="author" name="author" type="text" />
+        </div>
+    </form>
+  );
+}
