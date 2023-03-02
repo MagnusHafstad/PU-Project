ad264d7c1ea534a9fa54ad6405f9dbec51ced2ed
hermanak <hermanak@stud.ntnu.no>
2023-03-02 16:49:15 +0100

    style: #18 prettier complained about bad formatting

--- gruppe22/frontend/src/pages/insertbook/InsertBook.tsx
+++ gruppe22/frontend/src/pages/insertbook/InsertBook.tsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 export default function InsertBook() {
   return (
     <form>
-        <div>
-            <label htmlFor="title">Title</label>
-            <input id="title" name="title" type="text"/>
-        </div>
-        <div>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input id="title" name="title" type="text" />
+      </div>
+      <div>
         <label htmlFor="author">Author</label>
-            <input id="author" name="author" type="text" />
-        </div>
+        <input id="author" name="author" type="text" />
+      </div>
     </form>
   );
 }
