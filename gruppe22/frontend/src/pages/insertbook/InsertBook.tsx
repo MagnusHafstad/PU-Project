06ae86e9b40c45c4ca66c69025b6ffa3114ddebc
Hermann Andreas Kran <hermanakstud.ntnu.no>
2023-03-08 09:28:25 +0100

    feat: references added for the inputs and textarea of the form to insert a new book
    This will later be used to upload the data to firebase

--- gruppe22/frontend/src/pages/insertbook/InsertBook.tsx
+++ gruppe22/frontend/src/pages/insertbook/InsertBook.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./InsertBook.css";
 
 export default function InsertBook() {
+  const titleInputRef = useRef<HTMLInputElement>(null);
+  const authorInputRef = useRef<HTMLInputElement>(null);
+  const photoInputRef = useRef<HTMLInputElement>(null);
+  const descriptionInputRef = useRef<HTMLTextAreaElement>(null);
+  
+
+
   return (
     <form>
       <div>
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" type="text" />
+        <input id="title" name="title" type="text" ref={titleInputRef} />
       </div>
       <div>
         <label htmlFor="author">Author</label>
-        <input id="author" name="author" type="text" />
+        <input id="author" name="author" type="text" ref={authorInputRef} />
       </div>
       <div>
         <label htmlFor="photo">Photo</label>
-        <input id="photo" name="filename" type="file" accept="image/png, image/gif, image/jpeg" />
+        <input id="photo" name="filename" type="file" accept="image/png, image/gif, image/jpeg" ref={photoInputRef} />
       </div>
       {/* Might require newline */}
       <div>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" />
+        <textarea id="description" name="description" ref={descriptionInputRef} />
       </div>
       <button type="submit">Insert</button>
     </form>
