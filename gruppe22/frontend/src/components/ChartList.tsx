9eaed8dd517ff5ff0086b17ec93b1fb8da6687e7
Vetle Ekern <vetleek@gmail.com>
2023-03-20 17:40:49 +0100

    Filtering on genre and rating should be fixed now

--- gruppe22/frontend/src/components/ChartList.tsx
+++ gruppe22/frontend/src/components/ChartList.tsx
@@ -10,8 +10,9 @@ export default function BookList() {
   const colRef = collection(db, "books");
 
   const [books, setBooks] = React.useState<Book[] | undefined>();
-  const [defaultOption, setDefaultOption] = useState("Alle");
+  const [defaultOption, setDefaultOption] = useState("All");
   const [genres, setGenres] = React.useState<string[]>();
+  const [chartBooks, setChartBooks] = React.useState<Book[] | undefined>();
 
   async function fetchBooks() {
     getDocs(colRef).then((snapshot) => {
@@ -33,15 +34,6 @@ export default function BookList() {
     });
   }
 
-  useEffect(() => {
-    fetchBooks();
-    console.log(defaultOption);
-  }, []);
-
-  useEffect(() => {
-    books?.map((b) => getBookGenre(b.id));
-  }, [books]);
-
   const options = ["Alle", "Narnia", "fantasy"];
 
   function handleDropdownChange(selectedOption: any) {
@@ -61,49 +53,82 @@ export default function BookList() {
     return 0;
   }
 
-  async function getBookGenre(bookID: string) {
-    const genresTemp: string[] = [];
-    const genreRef = collection(db, "books/" + bookID + "/genres");
-    const snapshot = await getDocs(genreRef);
-    snapshot.docs.map((doc) => {
-      genresTemp.push(doc.get("genre"));
-    });
-    setGenres(genresTemp);
+  //sets genres to the unique genres from the sub-collection on each book
+  async function getBookGenre() {
+    const genresSet: Set<string> = new Set();
+    if (books && books.length > 0) {
+      await Promise.all(
+        books.map(async (b) => {
+          const genreRef = collection(db, "books/" + b.id + "/genres");
+          const snapshot = await getDocs(genreRef);
+          snapshot.docs.forEach((doc) => {
+            genresSet.add(doc.get("genre"));
+          });
+        })
+      );
+      setGenres(Array.from(genresSet)); // convert the set back to an array and set it as the state
+    }
   }
 
-  function hasGenre(bookId: string, defaultOption: string) {
-    const genreRef = collection(db, "books/" + bookId + "/genres");
-    const q = query(genreRef, where("genre", "==", defaultOption));
-    getDocs(q).then((snapshot) => {
-      console.log(snapshot.docs.length);
-      if (snapshot.docs.length > 0) {
-        return true;
-      } else {
-        return false;
-      }
-    });
+  async function hasGenre() {
+    const tempChartBooks: Book[] = [];
+    if (books && books.length > 0) {
+      await Promise.all(
+        books.map(async (b) => {
+          const genreRef = collection(db, "books/" + b.id + "/genres");
+          const snapshot = await getDocs(genreRef);
+          snapshot.docs.forEach((doc) => {
+            if (doc.get("genre") == defaultOption) {
+              tempChartBooks.push(b);
+            }
+          });
+        })
+      );
+      setChartBooks(tempChartBooks);
+    }
   }
+
+  useEffect(() => {
+    fetchBooks();
+    console.log(defaultOption);
+  }, []);
+
+  useEffect(() => {
+    console.log("get genres");
+    console.log(genres);
+    getBookGenre();
+  }, [books]);
+
+  useEffect(() => {
+    hasGenre();
+  }, [defaultOption]);
 
   return (
     <>
       <div className="BookList">
         <h1 className="ListTitle">Topplister</h1>
-        <Dropdown
-          options={options.map((option) => ({ value: option, label: option }))}
-          onChange={handleDropdownChange}
-          value={{ value: defaultOption, label: defaultOption }}
-          placeholder="Filter by category"
-        />
+        {genres != undefined && genres.length != 0 ? (
+          <Dropdown
+            options={genres.map((genre) => ({ value: genre, label: genre }))}
+            onChange={handleDropdownChange}
+            value={{ value: defaultOption, label: defaultOption }}
+            placeholder="Filter by category"
+          />
+        ) : (
+          <div>Loading dropdown...</div>
+        )}
+
         {books == undefined ? (
           <div>Loading...</div>
         ) : (
           <>
-            {books
-              .sort(compareBooks)
-              .filter((book) => defaultOption === "Alle" || genres?.includes(defaultOption)) //hasGenre(book.id, defaultOption)) //
-              .map((book) => (
-                <CompactSingleBook key={book.id} book={book} />
-              ))}
+            {chartBooks != undefined && chartBooks.length != 0 ? (
+              chartBooks.sort(compareBooks).map((book) => <CompactSingleBook key={book.id} book={book} />)
+            ) : defaultOption == "All" ? (
+              books.sort(compareBooks).map((book) => <CompactSingleBook key={book.id} book={book} />)
+            ) : (
+              <div>No books with this genre</div>
+            )}
           </>
         )}
       </div>
